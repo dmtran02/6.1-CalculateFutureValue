f168b8c4762d914c302a5d6b58f11340fe199355
dmtran02 <dmtran02@gmail.com>
2019-02-04 22:45:53 -0500

    added comments

--- FutureValue/Form1.cs
+++ FutureValue/Form1.cs
@@ -8,6 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * Duc Tran
+ * Professor Frank Friedman
+ * CIS 3309 - Assigment 6.2 // FutureValueCalculator
+ * Last Updated: February 4, 2019
+ */
+
 namespace FutureValue
 {
     public partial class Form1 : Form
@@ -16,6 +23,11 @@ namespace FutureValue
         {
             InitializeComponent();
         }
+
+        /*
+         * Calculates the Future Value using the CalculateFutureValue method that uses monthlyInvestment, months, monthlyInterestRate and futureValue
+         * that returns the futureValue value at the end of calculation upon clicking the button.
+         */
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
@@ -30,6 +42,12 @@ namespace FutureValue
             CalculateFutureValue(monthlyInvestment, months, monthlyInterestRate, futureValue);
         }
 
+        /*
+         * Method that assists in calculating the futureValue for the application
+         * Takes in four parameters: monthlyInvestment, months, monthlyInterestRate and futureValue
+         * At the end of calculation, the method focuses on the txtMonthlyInvestment text box.
+         */
+
         private void CalculateFutureValue(decimal monthlyInvestment, int months, decimal monthlyInterestRate, decimal futureValue)
         {
             for (int i = 0; i < months; i++)
@@ -41,10 +59,20 @@ namespace FutureValue
             }
         }
 
+        /*
+         * Event handler for when the user clicks the exit button.
+         * Exits the application upon clicking the button.
+         */
+
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
+        /*
+         * Clears the value in the futureValue text box when one of the three parameters, monthlyInvestment,
+         * months and monthlyInterestRate are text-changed.
+         */
 
         private void ClearFutureValue(object sender, EventArgs e)
         {
